9b9cf9ab0ec219aa8bbed3a4dc7fd6785368f986
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2022-11-14 16:59:52 -0500

    Harrison 585 - EBOY/JPTO - Wip Testing in cdf_variable_parser filter rules

--- src/cdf_variable_parser.py
+++ src/cdf_variable_parser.py
@@ -8,11 +8,23 @@ from spacepy import pycdf
 class CdfVariableParser:
 
     @staticmethod
+    def _check_needed_values_are_present(variable, cdf):
+        has_correct_shape = len(variable.shape) <= 2
+        has_field_name = 'FIELDNAM' in variable.attrs
+        time_col = variable.attrs['DEPEND_0']
+        has_time_delta_minus_col = 'DELTA_MINUS_VAR' in cdf[time_col].attrs
+        has_time_delta_plus_col = 'DELTA_PLUS_VAR' in cdf[time_col].attrs
+        zscale = variable.attrs['SCALETYP'] if 'SCALETYP' in variable.attrs else 'log'
+        is_z_scale_valid = zscale == 'linear' or variable.attrs['SCALEMIN'] != 0
+        # time_is_ns = cdf[time_col].attrs['UNITS'] == 'ns'
+        return has_correct_shape and has_field_name and has_time_delta_minus_col and has_time_delta_plus_col and is_z_scale_valid # and time_is_ns
+
+    @staticmethod
     def parse_variables_from_cdf(file_path: str) -> Dict[str, str]:
         cdf = pycdf.CDF(file_path)
         version = cdf.attrs['Data_version']
         return {f'{var.attrs["CATDESC"]} v{version}': key for key, var in cdf.items()
-                if var.attrs["VAR_TYPE"] == "data"}
+                if var.attrs["VAR_TYPE"] == "data" and CdfVariableParser._check_needed_values_are_present(var, cdf)}
 
     @staticmethod
     def parse_variables_from_cdf_bytes(cdf_bytes: bytes):
--- test/test_cdf_variable_parser.py
+++ test/test_cdf_variable_parser.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch, Mock
 
 from src.cdf_variable_parser import CdfVariableParser
 
@@ -52,3 +53,145 @@ class TestCdfVariableParser(unittest.TestCase):
 
         self.assertEqual(expected_descriptions, descriptions)
 
+    @patch('src.cdf_variable_parser.pycdf.CDF')
+    def test_parse_variables_from_cdf_bytes_filter_out_variables_that_are_missing_key_features(self, mock_CDF_class):
+        mock_cdf_instance = Mock()
+        mock_CDF_class.return_value = mock_cdf_instance
+
+        mock_cdf_instance.attrs = {'Data_version': "99"}
+
+        expected_descriptions = {'var_not_filtered v99': "var0"}
+
+        var_that_is_not_filtered = Mock()
+        var_that_is_not_filtered.attrs = {
+            "CATDESC": "var_not_filtered",
+            "VAR_TYPE": "data",
+            "FIELDNAM": "something",
+            "DEPEND_0": "time_col_good",
+            "SCALETYP": "linear",
+            "SCALEMIN": 1,
+        }
+        var_that_is_not_filtered.shape = (1, 2)
+
+        var_filtered_for_incorrect_shape = Mock()
+        var_filtered_for_incorrect_shape.attrs = {
+            "CATDESC": "var_filtered_for_incorrect_shape",
+            "VAR_TYPE": "data",
+            "FIELDNAM": "something",
+            "DEPEND_0": "time_col_good",
+            "SCALETYP": "linear",
+            "SCALEMIN": 1,
+        }
+        var_filtered_for_incorrect_shape.shape = (1, 2, 3)
+
+        var_filtered_for_missing_fieldnam = Mock()
+        var_filtered_for_missing_fieldnam.attrs = {
+            "CATDESC": "var_not_filtered",
+            "VAR_TYPE": "data",
+            "DEPEND_0": "time_col_good",
+            "SCALETYP": "linear",
+            "SCALEMIN": 1,
+        }
+        var_filtered_for_missing_fieldnam.shape = (1, 2)
+
+        var_filtered_for_missing_delta_minus = Mock()
+        var_filtered_for_missing_delta_minus.attrs = {
+            "CATDESC": "var_filtered",
+            "VAR_TYPE": "data",
+            "FIELDNAM": "something",
+            "DEPEND_0": "time_col_no_delta_minus",
+            "SCALETYP": "linear",
+            "SCALEMIN": 1,
+        }
+        var_filtered_for_missing_delta_minus.shape = (1, 2)
+
+        var_filtered_for_missing_delta_plus = Mock()
+        var_filtered_for_missing_delta_plus.attrs = {
+            "CATDESC": "var_filtered",
+            "VAR_TYPE": "data",
+            "FIELDNAM": "something",
+            "DEPEND_0": "time_col_no_delta_plus",
+            "SCALETYP": "linear",
+            "SCALEMIN": 1,
+        }
+        var_filtered_for_missing_delta_plus.shape = (1, 2)
+
+        var_filtered_for_linear_scaletype_and_scalemin_nonzero = Mock()
+        var_filtered_for_linear_scaletype_and_scalemin_nonzero.attrs = {
+            "CATDESC": "var_filtered",
+            "VAR_TYPE": "data",
+            "FIELDNAM": "something",
+            "DEPEND_0": "time_col_good",
+            "SCALETYP": "linear",
+            "SCALEMIN": 1,
+        }
+        var_filtered_for_linear_scaletype_and_scalemin_nonzero.shape = (1, 2)
+
+        var_filtered_for_scaletype_log_and_scalemin_zero = Mock()
+        var_filtered_for_scaletype_log_and_scalemin_zero.attrs = {
+            "CATDESC": "var_filtered",
+            "VAR_TYPE": "data",
+            "FIELDNAM": "something",
+            "DEPEND_0": "time_col_good",
+            "SCALETYP": "log",
+            "SCALEMIN": 0,
+        }
+        var_filtered_for_scaletype_log_and_scalemin_zero.shape = (1, 2)
+
+        var_filtered_for_scaletype_log_and_scalemin_nonzero = Mock()
+        var_filtered_for_scaletype_log_and_scalemin_nonzero.attrs = {
+            "CATDESC": "var_filtered",
+            "VAR_TYPE": "data",
+            "FIELDNAM": "something",
+            "DEPEND_0": "time_col_good",
+            "SCALETYP": "log",
+            "SCALEMIN": 1,
+        }
+        var_filtered_for_scaletype_log_and_scalemin_nonzero.shape = (1, 2)
+
+        var_filtered_for_scaletype_missing_and_scalemin_zero = Mock()
+        var_filtered_for_scaletype_missing_and_scalemin_zero.attrs = {
+            "CATDESC": "var_filtered",
+            "VAR_TYPE": "data",
+            "FIELDNAM": "something",
+            "DEPEND_0": "time_col_good",
+            "SCALEMIN": 0,
+        }
+        var_filtered_for_scaletype_missing_and_scalemin_zero.shape = (1, 2)
+
+        var_filtered_for_scaletype_missing_and_scalemin_nonzero = Mock()
+        var_filtered_for_scaletype_missing_and_scalemin_nonzero.attrs = {
+            "CATDESC": "var_filtered",
+            "VAR_TYPE": "data",
+            "FIELDNAM": "something",
+            "DEPEND_0": "time_col_good",
+            "SCALEMIN": 1,
+        }
+        var_filtered_for_scaletype_missing_and_scalemin_nonzero.shape = (1, 2)
+
+        mock_cdf_instance.items.return_value = {
+            'var0': var_that_is_not_filtered,
+            'var1': var_filtered_for_incorrect_shape,
+            'var2': var_filtered_for_missing_fieldnam,
+            'var3': var_filtered_for_missing_delta_minus,
+            'var4': var_filtered_for_missing_delta_plus,
+            'var5': var_filtered_for_linear_scaletype_and_scalemin_nonzero,
+            'var6': var_filtered_for_scaletype_log_and_scalemin_zero,
+            'var7': var_filtered_for_scaletype_log_and_scalemin_nonzero,
+            'var8': var_filtered_for_scaletype_missing_and_scalemin_zero,
+            'var9': var_filtered_for_scaletype_missing_and_scalemin_nonzero,
+        }.items()
+
+        mock_time_column_good = Mock()
+        mock_time_column_good.attrs = {'DELTA_MINUS_VAR', 'DELTA_PLUS_VAR'}
+        mock_time_col_no_delta_minus = Mock()
+        mock_time_col_no_delta_minus.attrs = {'DELTA_PLUS_VAR'}
+        mock_time_col_no_delta_plus = Mock()
+        mock_time_col_no_delta_plus.attrs = {'DELTA_MINUS_VAR'}
+        cdf_items = {'time_col_good': mock_time_column_good, 'time_col_no_delta_minus': mock_time_col_no_delta_minus, 'time_col_no_delta_plus': mock_time_col_no_delta_plus}
+        mock_cdf_instance.__getitem__ = Mock()
+        mock_cdf_instance.__getitem__.side_effect = lambda key: cdf_items[key]
+
+        returned_descriptions = CdfVariableParser.parse_variables_from_cdf("")
+
+        self.assertEqual(expected_descriptions, returned_descriptions)
