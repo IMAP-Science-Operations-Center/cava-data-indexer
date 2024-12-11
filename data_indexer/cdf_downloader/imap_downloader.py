a5fb3dcafcaf390d63a9e92888759c2a894614d3
Menlo Innovations - CAVA Project <harrison@menloinnovations.com>
2024-12-11 09:56:09 -0500

    Harrison 2212 - SWYN/GLUN/GSTU - implements IMAP processor to fetch from IMAP dev server

--- data_indexer/cdf_downloader/imap_downloader.py
+++ data_indexer/cdf_downloader/imap_downloader.py
@@ -1,11 +1,12 @@
 import json
 import ssl
 import urllib.request
+from typing import Any
+
 import certifi
 
 cdf_host = "http://3.139.73.210"
 metadata_path = "/dev/science-files-metadata"
-
 
 def get_all_metadata() -> [{}]:
     context = ssl.create_default_context(cafile=certifi.where())
@@ -16,4 +17,11 @@ def get_cdf_file(filename: str) -> dict:
     context = ssl.create_default_context(cafile=certifi.where())
     download_path = urllib.request.urlopen(cdf_host + "/dev/science-files-download?file=" + filename, context=context).read()
     link = cdf_host + download_path.decode("utf-8")
+
     return {"link": link, "data": urllib.request.urlopen(link, context=context).read()}
+
+sdc_dev_server = "https://api.dev.imap-mission.com/"
+
+def get_cdf_file_from_sdc(file_path: str) -> (str, Any):
+    url = sdc_dev_server + "download/" + file_path
+    return url, urllib.request.urlopen(url).read()
--- data_indexer/imap_data_processor.py
+++ data_indexer/imap_data_processor.py
@@ -1,12 +1,43 @@
+import re
 from collections import defaultdict
+from dataclasses import dataclass
 from datetime import datetime
+from functools import reduce
+from typing import TypeVar
 
-from data_indexer import utils, dates_available
-from data_indexer.cdf_downloader.imap_downloader import get_all_metadata, get_cdf_file
+import imap_data_access
+
+from data_indexer.cdf_downloader.imap_downloader import get_cdf_file_from_sdc
 from data_indexer.cdf_parser.cdf_parser import CdfParser
 from data_indexer.cdf_parser.variable_selector.default_variable_selector import DefaultVariableSelector
 from data_indexer.file_cadence.daily_file_cadence import DailyFileCadence
+from data_indexer.utils import get_index_entry
 
+
+@dataclass(frozen=True)
+class VariableIndex:
+    variable_name: str
+    catalog_description: str
+    display_type: str
+
+@dataclass(frozen=True)
+class Dataproduct:
+    instrument: str
+    data_level: str
+    descriptor: str
+
+@dataclass(frozen=True)
+class DataproductMetadata:
+    variables: list[VariableIndex]
+    source_file_format: str
+    description_source_file: str
+    logical_source: str
+    logical_source_description: str
+    version: str
+    generation_date: str
+    instrument: str
+    mission: str
+    file_cadence: str
 
 def group_metadata_by_file_names(metadata: [{}]) -> [{}]:
     grouped_files = defaultdict(list)
@@ -20,21 +51,46 @@ def group_metadata_by_file_names(metadata: [{}]) -> [{}]:
 
     return grouped_files
 
+T = TypeVar('T')
 
-def get_metadata_index():
+def flatten(l: list[list[T]]) -> list[T]:
+    return reduce(list.__add__, l, [])
+
+def get_metadata_index() -> list[dict]:
+    uuid_matcher = re.compile("-fake-menlo-[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}|-[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}")
+
+    l3_version_variants = ["l3", "l3a", "l3b"]
+    l3_cdf_metadatas = flatten([imap_data_access.query(data_level=version) for version in l3_version_variants])
+
+    data_products = defaultdict(lambda: defaultdict(list))
+    for cdf_metadata in l3_cdf_metadatas:
+        descriptor = cdf_metadata["descriptor"]
+        if "log" in descriptor or re.search(uuid_matcher, descriptor) is not None:
+            continue
+        # descriptor = re.sub(uuid_matcher, "", descriptor)
+
+        data_product = Dataproduct(cdf_metadata["instrument"], cdf_metadata["data_level"], descriptor)
+        data_products[data_product][cdf_metadata["version"]].append(cdf_metadata["file_path"])
+
+    print(data_products)
+
     index = []
-    metadata = group_metadata_by_file_names(get_all_metadata())
-    for file_name_format, matching_files_metadata in sorted(metadata.items()):
-        sorted_files_metadata = sorted(matching_files_metadata, key=lambda m: m["timetag"])
-        sorted_dates = [datetime.fromisoformat(metadata["timetag"]).date() for metadata in sorted_files_metadata]
-        available_dates = dates_available.get_date_ranges(sorted_dates)
-        first_file_metadata = matching_files_metadata[0]
-        cdf = get_cdf_file(first_file_metadata["file_name"])
-        cdf_file_info = CdfParser.parse_cdf_bytes(cdf["data"], DefaultVariableSelector)
-        link = cdf["link"].replace(first_file_metadata["file_name"], file_name_format)
-        instrument = _capitalize_isois_instrument_name(first_file_metadata["instrument_id"])
-        index.append(utils.get_index_entry(cdf_file_info, link, cdf["link"],
-                                           available_dates, instrument, "IMAP",DailyFileCadence))
+    for data_product, versions_to_files in data_products.items():
+        latest_version = max(versions_to_files.keys())
+        latest_files = versions_to_files[latest_version]
+        description_source_file = latest_files[0]
+
+        description_source_file_url, cdf = get_cdf_file_from_sdc(description_source_file)
+        cdf_file_info = CdfParser.parse_cdf_bytes(cdf, DefaultVariableSelector)
+
+        date_in_url_path_regex = re.compile("/[0-9]{4}/[0-9]{2}/")
+        date_in_file_name = re.compile("_[0-9]{8}_")
+
+        template_url = re.sub(date_in_url_path_regex, "/%yyyy%/%mm%/", description_source_file_url)
+        template_url = re.sub(date_in_file_name, "_%yyyymmdd%_", template_url)
+
+        index.append(get_index_entry(cdf_file_info, template_url, description_source_file_url, [], data_product.instrument, "IMAP", DailyFileCadence))
+
     return index
 
 def _capitalize_isois_instrument_name(instrument_name: str):
